5a3732d2ff1bd03c3518116f0d6b77bbe33a55cc
Andrei Lapets <alapets@bbn.com>
2015-11-11 14:58:29 -0500

    Update old testing script to use new conventions.

--- hw3-tests.py
+++ hw3-tests.py
@@ -1,7 +1,7 @@
 import re
 
 ############################################################
-# Load the files. Change the path if necessary.
+# Load the files.
 
 exec(open('parse.py').read())
 exec(open('interpret.py').read())
@@ -26,31 +26,16 @@ if compileCheck.find('import machine') != -1 or\
     print('''You did not load the machine.py module correctly in compile.py. You must use "exec(open('machine.py').read())". Exiting.''')
     exit()
 
-def check(name, function, inputs_result_pairs):
+def check(function, inputs_result_pairs):
     def str_(s): return '"'+str(s)+'"' if type(s) == str else str(s)
-    if type(name) == tuple:
-        prefix = name[0]
-        suffix = name[1]
-    if type(name) == str:
-        prefix = name + '('
-        suffix = ')'
-    
+    (prefix, suffix) = (function + '(', ')')
+
     passed = 0
     for (inputs, result) in inputs_result_pairs:
-        try:
-            if len(inputs) == 1:
-                output = function(inputs[0])
-            if len(inputs) == 2:
-                output = function(inputs[0], inputs[1])
-            if len(inputs) == 3:
-                output = function(inputs[0], inputs[1], inputs[2])
-        except:
-            output = None
-
-        if output == result:
-            passed = passed + 1
-        else:
-            print("\n  Failed on:\n    "+prefix+', '.join([str_(i) for i in inputs])+suffix+"\n\n"+"  Should be:\n    "+str(result)+"\n\n"+"  Returned:\n    "+str(output)+"\n")
+        try: output = eval(function)(*inputs)
+        except: output = None
+        if output == result: passed = passed + 1
+        else: print("\n  Failed on:\n    "+prefix+', '.join([str_(i) for i in inputs])+suffix+"\n\n"+"  Should be:\n    "+str(result)+"\n\n"+"  Returned:\n    "+str(output)+"\n")
     print("Passed " + str(passed) + " of " + str(len(inputs_result_pairs)) + " tests.")
     print("")
 
@@ -60,11 +45,11 @@ def check(name, function, inputs_result_pairs):
 print("Problem #1, part (a), evalTerm()...")
 try: evalTerm
 except: print("The evalTerm() function is not defined.")
-else: check('evalTerm', evalTerm, [\
-        (({}, {'Number': [123]}), {'Number': [123]}),\
-        (({}, {'Plus': [{'Number': [0]}, {'Number': [1]}]}), {'Number': [1]}),\
-        (({'x':{'Number':[123]}, 'y':{'Number':[123]}}, {'Plus': [{'Variable': ['x']}, {'Variable': ['y']}]}), {'Number': [246]}),\
-        ])
+else: check('evalTerm', [\
+    (({}, {'Number': [123]}), {'Number': [123]}),\
+    (({}, {'Plus': [{'Number': [0]}, {'Number': [1]}]}), {'Number': [1]}),\
+    (({'x':{'Number':[123]}, 'y':{'Number':[123]}}, {'Plus': [{'Variable': ['x']}, {'Variable': ['y']}]}), {'Number': [246]}),\
+    ])
 
 def testExecProgram(env, stmt):
     return execProgram(env, stmt)[1]
@@ -72,20 +57,20 @@ def testExecProgram(env, stmt):
 print("Problem #1, part (c), execProgram()...")
 try: execProgram
 except: print("The execProgram() function is not defined.")
-else: check('testExecProgram', testExecProgram, [\
-        (({'y':'False'}, {'Print': [{'Not':[{'Variable':['y']}]}, 'End']}), ['True']),\
-        (({}, {'If': ['True', {'Print': [{'Number': [4]}, 'End']}, 'End']}), [{'Number':[4]}]),\
-        (({}, {'Assign': [{'Variable': ['x']}, 'False', {'Until': [{'Not': [{'Variable': ['x']}]}, {'Print': ['False', 'End']}, {'Print': ['True', 'End']}]}]}), ['True']),\
-        (({}, {'If': ['True', {'Print': ['True', 'End']}, {'Print': ['False', 'End']}]}), ['True', 'False']),\
-        (({}, {'Procedure': [{'Variable': ['example']}, {'Print': [{'Number': [4]}, 'End']}, {'Call': [{'Variable': ['example']}, 'End']}]}), [{'Number':[4]}]),\
-        (({}, {'Assign': [{'Variable': ['x']}, {'Number': [123]}, {'Procedure': [{'Variable': ['example']}, {'Print': [{'Variable': ['x']}, 'End']}, {'Call': [{'Variable': ['example']}, {'Call': [{'Variable': ['example']}, 'End']}]}]}]}), [{'Number':[123]}, {'Number':[123]}]),\
-        (({}, {'Procedure': [{'Variable': ['g']}, {'Print': [{'Number': [2]}, 'End']}, {'Procedure': [{'Variable': ['f']}, {'Call': [{'Variable': ['g']}, {'Print': [{'Number': [1]}, {'Call': [{'Variable': ['g']}, 'End']}]}]}, {'Call': [{'Variable': ['f']}, 'End']}]}]}), [{'Number':[2]}, {'Number':[1]}, {'Number':[2]}]),\
-        ])
+else: check('testExecProgram', [\
+    (({'y':'False'}, {'Print': [{'Not':[{'Variable':['y']}]}, 'End']}), ['True']),\
+    (({}, {'If': ['True', {'Print': [{'Number': [4]}, 'End']}, 'End']}), [{'Number':[4]}]),\
+    (({}, {'Assign': [{'Variable': ['x']}, 'False', {'Until': [{'Not': [{'Variable': ['x']}]}, {'Print': ['False', 'End']}, {'Print': ['True', 'End']}]}]}), ['True']),\
+    (({}, {'If': ['True', {'Print': ['True', 'End']}, {'Print': ['False', 'End']}]}), ['True', 'False']),\
+    (({}, {'Procedure': [{'Variable': ['example']}, {'Print': [{'Number': [4]}, 'End']}, {'Call': [{'Variable': ['example']}, 'End']}]}), [{'Number':[4]}]),\
+    (({}, {'Assign': [{'Variable': ['x']}, {'Number': [123]}, {'Procedure': [{'Variable': ['example']}, {'Print': [{'Variable': ['x']}, 'End']}, {'Call': [{'Variable': ['example']}, {'Call': [{'Variable': ['example']}, 'End']}]}]}]}), [{'Number':[123]}, {'Number':[123]}]),\
+    (({}, {'Procedure': [{'Variable': ['g']}, {'Print': [{'Number': [2]}, 'End']}, {'Procedure': [{'Variable': ['f']}, {'Call': [{'Variable': ['g']}, {'Print': [{'Number': [1]}, {'Call': [{'Variable': ['g']}, 'End']}]}]}, {'Call': [{'Variable': ['f']}, 'End']}]}]}), [{'Number':[2]}, {'Number':[1]}, {'Number':[2]}]),\
+    ])
 
 print("Problem #1, interpret()...")
 try: interpret
 except: print("The interpret() function is not defined.")
-else: check('interpret', interpret, [\
+else: check('interpret', [\
     (("print 123;",), [{'Number':[123]}]),\
     (("print false; print true; print 4;",), ['False', 'True', {'Number':[4]}]),\
     (("x := 10; print x;",), [{'Number':[10]}]),\
@@ -105,7 +90,7 @@ else: check('interpret', interpret, [\
     (("procedure g {print 2;} procedure f {call g; print 1; call g;} call f;",), [{'Number':[2]},{'Number':[1]},{'Number':[2]}]),\
     (("procedure g {print 2;} if true and true { call g; }",), [{'Number':[2]}]),\
     (("procedure g {print 2;} procedure f {if true and true { call g; }} call g; call f;",), [{'Number':[2]},{'Number':[2]}]),\
-    (("procedure h {print 3;} procedure g {print 2; call h; call h;} procedure f {call g; print 1; call g;} call f;",), [{'Number':[2]},{'Number':[3]},{'Number':[3]},{'Number':[1]},{'Number':[2]},{'Number':[3]},{'Number':[3]}])\
+    (("procedure h {print 3;} procedure g {print 2; call h; call h;} procedure f {call g; print 1; call g;} call f;",), [{'Number':[2]},{'Number':[3]},{'Number':[3]},{'Number':[1]},{'Number':[2]},{'Number':[3]},{'Number':[3]}]),\
     ])
 
 def testIncrement(addr, initial):
@@ -114,17 +99,20 @@ def testIncrement(addr, initial):
 print("Problem #2, part (a), increment()...")
 try: increment
 except: print("The increment() function is not defined.")
-else: check('testIncrement', testIncrement, [\
-        ((9, 123), [124]),\
-        ((20, 234), [235]),\
-        ((100, 0), [1]),\
-        ((1000, 1), [2])\
-        ])
+else: check('testIncrement', [\
+    ((9, 123), [124]),\
+    ((20, 234), [235]),\
+    ((100, 0), [1]),\
+    ((1000, 1), [2]),\
+    ])
+
+def testCompile(s):
+    return simulate(compile(s))
 
 print("Problem #3, part (d), compile()...")
 try: compile
 except: print("The compile() function is not defined.")
-else: check(('simulate(compile(', '))'), lambda s: simulate(compile(s)), [\
+else: check('testCompile', [\
     (("print 123;",), [123]),\
     (("print false; print true; print 4;",), [0, 1, 4]),\
     (("x := 10; print x;",), [10]),\
@@ -146,7 +134,7 @@ else: check(('simulate(compile(', '))'), lambda s: simulate(compile(s)), [\
     (("procedure g {print 2;} procedure f {call g; print 1; call g;} call f;",), [2,1,2]),\
     (("procedure g {print 2;} if true and true { call g; }",), [2]),\
     (("procedure g {print 2;} procedure f {if true and true { call g; }} call g; call f;",), [2,2]),\
-    (("procedure h {print 3;} procedure g {print 2; call h; call h;} procedure f {call g; print 1; call g;} call f;",), [2,3,3,1,2,3,3])\
+    (("procedure h {print 3;} procedure g {print 2; call h; call h;} procedure f {call g; print 1; call g;} call f;",), [2,3,3,1,2,3,3]),\
     ])
 
 #eof
